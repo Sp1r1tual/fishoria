a5eb707b44e2b007299ff50177b166dd74ab1a4f
Sp1r1tual <andrii0383@gmail.com>
2026-04-08 01:19:52 +0300

    fix(client): missing footer on achievements rpage

--- client/src/router/pages/AchievementsPage.tsx
+++ client/src/router/pages/AchievementsPage.tsx
@@ -1,5 +1,10 @@
 import { Achievements } from '@/components/Achievements/Achievements';
+import { PageLayout } from '../../layouts/PageLayout';
 
 export const AchievementsPage = () => {
-  return <Achievements />;
+  return (
+    <PageLayout>
+      <Achievements />
+    </PageLayout>
+  );
 };
