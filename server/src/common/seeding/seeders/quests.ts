7a67d8c1bdae3649a56bef32fdb74cf5b8a1bad2
Sp1r1tual <andrii0383@gmail.com>
2026-04-24 12:22:49 +0300

    feat(api): add resetting quest progress when updating them

--- server/src/common/seeding/seeders/quests.ts
+++ server/src/common/seeding/seeders/quests.ts
@@ -400,6 +400,15 @@ export async function seedQuests(prisma: PrismaClient) {
     };
 
     if (existing) {
+      await prisma.playerQuest.updateMany({
+        where: { questId: existing.id },
+        data: {
+          isCompleted: false,
+          isClaimed: false,
+          progress: {},
+        },
+      });
+
       await prisma.quest.update({
         where: { id: existing.id },
         data: {
